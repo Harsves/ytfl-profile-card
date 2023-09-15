cf8e11de4770fbb64686f4a92d4a3db4388b1326
Suuskehh <suus.harsveld@hva.nl>
2023-09-15 14:07:59 +0200

    Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,6 @@ import { SliceZone } from '@prismicio/svelte'
 import { components } from '$lib/slices'
 
 export let data
-// console.log(data)
 </script>
 
 <SliceZone slices={data.slices} {components} />
